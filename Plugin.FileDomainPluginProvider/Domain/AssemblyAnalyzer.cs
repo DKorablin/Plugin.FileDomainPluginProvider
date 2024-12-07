dc7c950ff47e59ed704d1e7471756aa6a74357bd
DKorablin2 <danila.korablin@unity3d.com>
2024-12-07 14:10:51 +0300

    Fixed logic to check for plugin extensions

--- Plugin.FileDomainPluginProvider/Domain/AssemblyAnalyzer.cs
+++ Plugin.FileDomainPluginProvider/Domain/AssemblyAnalyzer.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Threading;
+using Plugin.FilePluginProvider;
 
 namespace Plugin.FileDomainPluginProvider.Domain
 {
@@ -17,15 +18,16 @@ namespace Plugin.FileDomainPluginProvider.Domain
 			{
 				List<ManualResetEvent> onDone = new List<ManualResetEvent>();
 				List<AssemblyTypesReader> readers = new List<AssemblyTypesReader>();
-				foreach(String filePath in Directory.GetFiles(path, Constant.LibrarySearchExtension, SearchOption.AllDirectories))
-				{
-					ManualResetEvent evt = new ManualResetEvent(false);
-					AssemblyTypesReader reader = new AssemblyTypesReader(new String[] { filePath }, evt);
-					onDone.Add(evt);
-					readers.Add(reader);
+				foreach(String filePath in Directory.GetFiles(path, "*.*", SearchOption.AllDirectories))
+					if(FilePluginArgs.CheckFileExtension(filePath))
+					{
+						ManualResetEvent evt = new ManualResetEvent(false);
+						AssemblyTypesReader reader = new AssemblyTypesReader(new String[] { filePath }, evt);
+						onDone.Add(evt);
+						readers.Add(reader);
 
-					ThreadPool.QueueUserWorkItem(reader.Read, this);
-				}
+						ThreadPool.QueueUserWorkItem(reader.Read, this);
+					}
 
 				foreach(ManualResetEvent evt in onDone)
 					evt.WaitOne();
@@ -52,7 +54,7 @@ namespace Plugin.FileDomainPluginProvider.Domain
 			AssemblyTypesInfo result = null;
 			try
 			{
-				if(Path.GetExtension(assemblyPath).Equals(Constant.LibraryExtension, StringComparison.OrdinalIgnoreCase))
+				if(FilePluginArgs.CheckFileExtension(assemblyPath))
 					result = AssemblyTypesReader.GetAssemblyTypes(assemblyPath);
 			} finally
 			{
