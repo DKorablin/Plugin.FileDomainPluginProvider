bbb8958d0ca7918614408f9d85a9e9953aab8418
DKorablin2 <danila.korablin@unity3d.com>
2025-02-23 16:45:09 +0300

    Unify plugins build

--- Plugin.FileDomainPluginProvider/Properties/AssemblyInfo.cs
+++ Plugin.FileDomainPluginProvider/Properties/AssemblyInfo.cs
@@ -1,7 +1,6 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: ComVisible(false)]
 [assembly: Guid("9b495690-d55f-4aa0-9e44-93787550a2aa")]
 [assembly: System.CLSCompliant(true)]
 
@@ -9,14 +8,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyMetadata("ProjectUrl", "https://dkorablin.ru/project/Default.aspx?File=106")]
 #else
 
-[assembly: AssemblyTitle("Plugin.FileDomainPluginProvider")]
 [assembly: AssemblyDescription("Plugin loader assemblty with sandbox check in new domain")]
-#if DEBUG
-[assembly: AssemblyConfiguration("Debug")]
-#else
-[assembly: AssemblyConfiguration("Release")]
-#endif
-[assembly: AssemblyCompany("Danila Korablin")]
-[assembly: AssemblyProduct("File Domain Plugin Provider")]
-[assembly: AssemblyCopyright("Copyright © Danila Korablin 2016-2024")]
+[assembly: AssemblyCopyright("Copyright © Danila Korablin 2016-2025")]
 #endif
