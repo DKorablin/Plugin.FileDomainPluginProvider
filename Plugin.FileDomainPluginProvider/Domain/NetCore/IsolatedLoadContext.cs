484fac5a676bb7133d46c2cd4c4a125ca62489d8
DKorablin2 <danila.korablin@unity3d.com>
2025-10-26 13:17:33 +0300

    Partially added logic to rresolve assemblies from shared nuget folder

--- Plugin.FileDomainPluginProvider/Domain/NetCore/IsolatedLoadContext.cs
+++ Plugin.FileDomainPluginProvider/Domain/NetCore/IsolatedLoadContext.cs
@@ -1,4 +1,3 @@
-// net5+ only implementation. For net35 this file is empty.
 #if NET5_0_OR_GREATER
 using System;
 using System.IO;
@@ -10,23 +9,64 @@ namespace Plugin.FileDomainPluginProvider.Domain.NetCore
 	/// <summary>Custom AssemblyLoadContext that isolates plugin assemblies for .NET 5+ targets.</summary>
 	internal sealed class IsolatedLoadContext : AssemblyLoadContext
 	{
+		private static Boolean DependencyContextFailed = false;
+
 		private readonly String _baseDirectory;
+		private String _pluginPath;
+		private AssemblyDependencyResolver _resolver;
+		private NuGetResolver _extendedResolver;
+
+		private NuGetResolver ExtendedResolver
+		{
+			get => this._extendedResolver ??= new NuGetResolver(this._pluginPath);
+		}
 
 		public IsolatedLoadContext(String baseDirectory, Boolean isCollectible)
-			: base($"PluginContext_{Guid.NewGuid():N}", isCollectible)
+			: base($"Plugin.FileDomainPluginProvider.ProbingContext.{Guid.NewGuid():N}", isCollectible)
 		{
 			_baseDirectory = baseDirectory ?? throw new ArgumentNullException(nameof(baseDirectory));
 		}
 
+		public Assembly LoadFromPluginPath2(String pluginPath)
+		{
+			this._pluginPath = pluginPath ?? throw new ArgumentNullException(nameof(pluginPath));
+			this._extendedResolver = null;
+
+			this._resolver = new AssemblyDependencyResolver(pluginPath);
+			return this.LoadFromAssemblyPath(pluginPath);
+		}
+
 		protected override Assembly Load(AssemblyName assemblyName)
 		{
-			String candidatePath = Path.Combine(_baseDirectory, assemblyName.Name + ".dll");
-			if(File.Exists(candidatePath))
-			{
-				try { return LoadFromAssemblyPath(candidatePath); }
-				catch(BadImageFormatException) { return null; }
-			}
+			String candidatePath = _resolver.ResolveAssemblyToPath(assemblyName);
+			if(candidatePath != null)
+				return TryToLoadAssembly(candidatePath);
+
+			if(!DependencyContextFailed)
+				try
+				{
+					candidatePath = this.ExtendedResolver?.ResolveAssemblyToPath(assemblyName);
+				} catch(FileNotFoundException exc)
+				{// ignore, referenced assemblies are not loaded from NuGet packages
+					DependencyContextFailed = true;
+				}
+
+			if(candidatePath != null)
+				return TryToLoadAssembly(candidatePath);
+
 			return null; // delegate to default context
+
+			Assembly TryToLoadAssembly(String path)
+			{
+				try
+				{
+					return LoadFromAssemblyPath(path);
+				}
+				catch(BadImageFormatException)
+				{
+					return null;
+				}
+			}
 		}
 	}
 }
